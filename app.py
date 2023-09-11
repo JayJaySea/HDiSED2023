0ee1162c36cc1e8c004b9821226446ced6a29d26
JayJaySea <xkalibur@wp.pl>
2023-09-11 14:09:37 +0200

    Finish basic API

--- app.py
+++ app.py
@@ -16,6 +16,42 @@ def games(offset, limit):
 
     return to_objects(columns, games)
 
+@app.route("/games/player/<id>", methods = ["GET"])
+def games_by_player(id):
+    cur = get_db_cursor()
+
+    columns = table_columns(cur, "games")
+    res = cur.execute("select * from games where white_id = ? or black_id = ?", (id, id))
+    games = res.fetchall()
+
+    cur.close()
+
+    return to_objects(columns, games)
+
+@app.route("/games/tournament/<id>", methods = ["GET"])
+def games_by_tournament(id):
+    cur = get_db_cursor()
+
+    columns = table_columns(cur, "games")
+    res = cur.execute("select * from games where tournament_id = ?", (id,))
+    games = res.fetchall()
+
+    cur.close()
+
+    return to_objects(columns, games)
+
+@app.route("/game/<id>", methods = ["GET"])
+def game_by_id(id: str):
+    cur = get_db_cursor()
+
+    columns = table_columns(cur, "games")
+    res = cur.execute("select * from games where id = ?", (id,))
+    game = res.fetchall()
+
+    cur.close()
+
+    return to_objects(columns, game)
+
 @app.route("/players/<offset>/<limit>", methods = ["GET"])
 def players(offset, limit):
     cur = get_db_cursor()
@@ -28,10 +64,33 @@ def players(offset, limit):
 
     return to_objects(columns, players)
 
+@app.route("/players/tournament/<id>", methods = ["GET"])
+def players_by_tournament(id):
+    cur = get_db_cursor()
+
+    query = """
+select distinct white_id from games where tournament_id = ?
+UNION
+select distinct black_id from games where tournament_id = ?
+"""
+    res = cur.execute(query, (id, id))
+    player_ids = res.fetchall()
+    player_ids = [t[0] for t in player_ids]
+
+    placeholders = ', '.join(['?'] * len(player_ids))
+    query = f"select distinct * from players where id in ({placeholders})"
+
+    res = cur.execute(query, player_ids)
+    tournaments = res.fetchall()
+
+    columns = table_columns(cur, "players")
+    cur.close()
+
+    return to_objects(columns, tournaments)
+
 @app.route("/player/<id>", methods = ["GET"])
 def player_by_id(id: str):
     cur = get_db_cursor()
-    print(id)
 
     columns = table_columns(cur, "players")
     res = cur.execute("select * from players where id = ?", (id,))
@@ -54,10 +113,28 @@ def tournaments(offset, limit):
     return to_objects(columns, tournaments)
 
 
+@app.route("/tournaments/player/<id>", methods = ["GET"])
+def tournaments_by_player(id):
+    cur = get_db_cursor()
+
+    columns = table_columns(cur, "tournaments")
+    res = cur.execute("select distinct tournament_id from games where white_id = ? or black_id = ?", (id, id))
+    tournament_ids = res.fetchall()
+    tournament_ids = [t[0] for t in tournament_ids]
+
+    placeholders = ', '.join(['?'] * len(tournament_ids))
+    query = f"select * from tournaments where id in ({placeholders})"
+
+    res = cur.execute(query, tournament_ids)
+    tournaments = res.fetchall()
+
+    cur.close()
+
+    return to_objects(columns, tournaments)
+
 @app.route("/tournament/<id>", methods = ["GET"])
 def tournament_by_id(id: str):
     cur = get_db_cursor()
-    print(id)
 
     columns = table_columns(cur, "tournaments")
     res = cur.execute("select * from tournaments where id = ?", (id,))
